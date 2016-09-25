07462ea01691cf020d03a4373916d207c75ca600
jmz <aiyoudqrjmz@163.com>
2016-09-25 22:11:22 +0800

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,91 @@
  import './mod1.js';
 
  //引入react组件 
- import jsx from './index.jsx';
+ // import jsx from './index.jsx';
+
+
+ // 引入redux
+//   import Redux from 'redux';
+
+// /** Action Creators */
+// function inc() {
+//   return { type: 'INCREMENT' };
+// }
+// function dec() {
+//   return { type: 'DECREMENT' };
+// }
+
+// function reducer(state, action) {
+//   // 首次调用本函数时设置初始 state
+//   state = state || { counter: 0 };
+
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { counter: state.counter + 1 };
+//     case 'DECREMENT':
+//       return { counter: state.counter - 1 };
+//     default:
+//       return state; // 无论如何都返回一个 state
+//   }
+// }
+
+// // 创建一个store
+// var store = Redux.createStore(reducer);
+
+// console.log( store.getState() ); // { counter: 0 }
+
+// store.dispatch(inc());
+// console.log( store.getState() ); // { counter: 1 }
+
+// store.dispatch(inc());
+// console.log( store.getState() ); // { counter: 2 }
+
+// store.dispatch(dec());
+// console.log( store.getState() ); // { counter: 1 }
+
+
+import { createStore } from 'redux';
+
+/**
+ * 这是一个 reducer，形式为 (state, action) => state 的纯函数。
+ * 描述了 action 如何把 state 转变成下一个 state。
+ *
+ * state 的形式取决于你，可以是基本类型、数组、对象、
+ * 甚至是 Immutable.js 生成的数据结构。惟一的要点是
+ * 当 state 变化时需要返回全新的对象，而不是修改传入的参数。
+ *
+ * 下面例子使用 `switch` 语句和字符串来做判断，但你可以写帮助类(helper)
+ * 根据不同的约定（如方法映射）来判断，只要适用你的项目即可。
+ */
+function counter(state = 0, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1;
+  case 'DECREMENT':
+    return state - 1;
+  default:
+    return state;
+  }
+}
+
+// 创建 Redux store 来存放应用的状态。
+// API 是 { subscribe, dispatch, getState }。
+let store = createStore(counter);
+
+// 可以手动订阅更新，也可以事件绑定到视图层。
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+// 改变内部 state 惟一方法是 dispatch 一个 action。
+// action 可以被序列化，用日记记录和储存下来，后期还可以以回放的方式执行
+store.dispatch({ type: 'increment' });
+// 1
+store.dispatch({ type: 'INCREMENT' });
+// 2
+store.dispatch({ type: 'DECREMENT' });
+// 1
+store.dispatch({ type: 'jmz' });
  
 
 

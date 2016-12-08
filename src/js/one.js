77cf12dc4e762e57c96045a56971098b12d5b8e6
研发中心-季明壮18827 <jmz18827@tcent.cn>
2016-12-08 10:09:16 +0800

    web组件问题

--- src/js/one.js
+++ src/js/one.js
@@ -1,9 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+
+// 专题页面
 import './react.js'
 
 
-
-
 // tab组件
-// import './components/tab.jsx'
+import './components/tab.jsx';
+
+
+ 
+// import '../css/test.css';
+

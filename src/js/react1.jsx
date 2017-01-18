e344e5abe441af56d12d2ffde50929dbda1164e2
jmz <aiyoudqrjmz@163.com>
2017-01-18 20:33:45 +0800

    test

--- src/js/react1.jsx
+++ src/js/react1.jsx
@@ -1,27 +1,32 @@
-var App=React.createClass({
-    getDefaultProps:function(){
-        console.log('默认属性');
+var App = React.createClass({
+    getDefaultProps: function () {
+        return {};
     },
-    getInitialState:function(){
-        console.log('初始化组件状态');
-        return {
-
+    getInitialState: function () {
+        return {};
+    },
+    render: function () {
+        var styleObj = {
+            width: '200px',
+            height: '200px',
+            background: this.props.colors,
+            borderRadius: '100%',
+            display:'inline-block'
         };
-    },
-    componentWillMount:function(){
-        console.log('组件将要被装载');
-    },
-    componentDidMount:function(){
-        console.log('组件已经被装载');
-    },
-    render:function(){
-        console.log('组件进行渲染,渲染完成后组件才会被真正装载');
         return (
-            <div>
-                渲染组件完成来拉来
-            </div>
+            <div style={styleObj}></div>
         );
     }
 });
 
-ReactDOM.render(<App />,document.querySelector('#app'));
+var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363",
+              "#85FFC7", "#297373", "#FF8552", "#A40E4C"],
+    reactComp=[];
+
+for(var i=0;i<colors.length;i++){
+    reactComp.push(<App key={i} colors={colors[i]} />);
+}
+
+ReactDOM.render(<div>
+{reactComp}
+</div>,document.querySelector('#app'));

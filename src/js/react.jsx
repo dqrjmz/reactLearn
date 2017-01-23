97df6a27e84aea88d2ee914f8b713df9fabdadc9
jmz <jmz18827@ly.com>
2017-01-23 12:31:52 +0800

    test

--- src/js/react.jsx
+++ src/js/react.jsx
@@ -1,29 +1,77 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-
-// import { HeaderComponent } from './workComponent/HeaderComponent.jsx';
-
-
-
-// // 样式模块
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+// 样式模块
 // import '../css/index.css'
 
 // var App = React.createClass({
 //     render: function () {
 //         return (
-//            <HeaderComponent  />
+//           <div>
+              
+//           </div>
 //         );
 //     }
 // });
 
-// ReactDOM.render(<App />, document.querySelector('#app'));
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-// var H1=React.createElement('h1',{title:'jmz'},'jmz');
+// // 定义一个虚拟dom生成器
+// //view层
+// const Counter=((value)=>{
+//     <h1>{value}</h1>
+// });
 
-// ReactDOM.render(<H1 />,document.querySelector('#app'));
+// // 根据action，和当前state,对state进行操作，返回新的state
+// const reducer=(state=0,action)=>{
+//     switch(action.type){
+//         case 'ADD':return state+1;
+//         case 'REDUCE':return state-1;
+//         default:return state;
+//     }
+// }
 
-//   alert('df');
+// // 创建一个状态容器
+// const store=createStore(reducer);
+
+
+// const render=()=>{
+//     ReactDOM.render(<Counter value={store.getState()} />,document.querySelector('#app'));
+// }
+
+// store.subscribe(render);
+// render();
+
+const Counter = (value ) => {
+    <div>
+        
+    
+  <h1>{value}</h1>
+  <button onClick={onIncrement}>+</button>
+  <button onClick={onDecrement}>-</button>
+  </div>
+}
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />,
+    document.getElementById('root')
+  );
+};
+
+render();
+store.subscribe(render);

707bb092264ed0501392a6fc649ba01bb0743687
jmz <jmz18827@ly.com>
2017-02-06 18:29:50 +0800

    tes

--- src/js/react.jsx
+++ src/js/react.jsx
@@ -1,30 +1,48 @@
-/*// 这里写的是 容器组件；
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+/*import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
 
-// 展示组件
-import Counter from './components/Counter';
+const store = createStore(reducer)
 
-//reducers 
-import counter from './reducers';
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)*/
 
-const store = createStore(counter);
-const rootEl=document.querySelector('#app');
+// $('a').my()   $('a')实例化 
 
-const render=()=>ReactDOM.render(
-    <Counter
-        value={store.getState()}
-        onIncrement={()=>{store.dispatch({type:'INCREMENT'})}}
-        onDecrement={()=>{store.dispatch({type:'DECREMENT'})}}
-     />
-    ,rootEl
-);
+/**
+ * $.fn==jQuery.prototype
+ * this:执行$('#app') 实例【jQuery函数实例】
+ * 插件函数中的回调函数中的this指向nation dom
+ */
+(function ($) {
+  $.fn.myPlugin = function (options) {
 
-//订阅事件
-store.subscribe(render);*/
+    // 获取这个dom节点
+    console.log(this.css({ width: '20px', height: '20px', backgroundColor: 'red' }));
 
+    const defaultOptions = {
+      'location': 'top',
+      'background-color': 'red'
+    };
+    //对这个dom节点做的自定义操作配置
+    //第一传递{}对象，为的是不改变defaultOptions，默认值；
+    const settings = $.extend({}, defaultOptions, options);
 
+    //每次返回要返回this：当前jquery元素；
+    //对这个dom节点做得统一操作；
+    return this.each((index, el) => {
+      $(el).css('color', 'white');
+    });
+  }
+})(jQuery);
 
-
+const a = $('a').myPlugin();
+console.log(a);
 
